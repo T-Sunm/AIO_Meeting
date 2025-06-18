87c946728db391ff21b10a512668609c17f80222
Tran Quang Minh <moonlig73@gmail.com>
2025-06-18 22:22:22 +0700

    add resource rag

--- MLOPs5_RAGAgents/AirFlow-AIVN/platform/dags/deploy.sh
+++ MLOPs5_RAGAgents/AirFlow-AIVN/platform/dags/deploy.sh
@@ -5,11 +5,13 @@ AIRFLOW_DIR="../airflow/"
 DAGS_DIR="../airflow/dags/"
 CONFIG_DIR="../airflow/config/"
 
-# Sync .py files to dags folder
-rsync -av --progress --include='*.py' --exclude='*' "$SOURCE_DIR" "$DAGS_DIR"
+# Copy all .py from SOURCE_DIR to DAGS_DIR
+mkdir -p "$DAGS_DIR"
+cp -v "$SOURCE_DIR"/*.py "$DAGS_DIR"
 
-# Sync .yaml files to config folder
-rsync -av --progress --include='*.yaml' --exclude='*' "$SOURCE_DIR" "$CONFIG_DIR"
+# Copy all .yaml from SOURCE_DIR to CONFIG_DIR
+mkdir -p "$CONFIG_DIR"
+cp -v "$SOURCE_DIR"/*.yaml "$CONFIG_DIR"
 
-# Sync requirements.txt to config folder
-rsync -av --progress --include='requirements.txt' --exclude='*' "$SOURCE_DIR" "$AIRFLOW_DIR"
+# Copy requirements.txt to AIRFLOW_DIR
+cp -v "$SOURCE_DIR"/requirements.txt "$AIRFLOW_DIR"
